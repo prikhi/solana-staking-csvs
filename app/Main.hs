7904622a5e71f8d2258d151416e16632d2f426d6
Pavan Rikhi <pavan.rikhi@gmail.com>
2021-08-06 23:04:38 -0400

    Add CLI Argument Parsing With cmdargs

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,9 @@
 module Main where
 
-import           Console.SolanaStaking.Main     ( run )
+import           Console.SolanaStaking.Main     ( getArgs
+                                                , run
+                                                )
 
-import           System.Environment             ( getArgs )
 
 main :: IO ()
-main = getArgs >>= \case
-    [apiKey, accountPubKey] -> run apiKey accountPubKey
-    _ ->
-        error
-            "solana-staking-csvs: expected 2 arguments - <API_KEY> <ACCOUNT_PUBKEY>"
+main = getArgs >>= run
--- src/Console/SolanaStaking/Main.hs
+++ src/Console/SolanaStaking/Main.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE RecordWildCards #-}
 {-| This module contains the @main@ function used by the exectuable.
 
 -}
 module Console.SolanaStaking.Main
     ( run
+    , getArgs
+    , Args(..)
     ) where
 
 import           Control.Monad                  ( (<=<)
@@ -23,11 +27,26 @@ import           Data.Time.Clock.POSIX          ( posixSecondsToUTCTime )
 import           Data.Time.Format               ( defaultTimeLocale
                                                 , formatTime
                                                 )
+import           Data.Version                   ( showVersion )
+import           System.Console.CmdArgs         ( (&=)
+                                                , Data
+                                                , Typeable
+                                                , argPos
+                                                , cmdArgs
+                                                , def
+                                                , help
+                                                , helpArg
+                                                , name
+                                                , program
+                                                , summary
+                                                , typ
+                                                )
 import           System.IO                      ( hPutStrLn
                                                 , stderr
                                                 )
 
 import           Console.SolanaStaking.Api
+import           Paths_solana_staking_csvs      ( version )
 
 import qualified Data.Text                     as T
 import qualified Data.Text.IO                  as T
@@ -36,8 +55,8 @@ import qualified Data.Text.IO                  as T
 -- | Pull staking rewards data for the account & print a CSV to stdout.
 --
 -- TODO: concurrent/pooled requests, but w/ 100 req/s limit
-run :: String -> String -> IO ()
-run apiKey accountPubKey = either (error . show) return <=< runner $ do
+run :: Args -> IO ()
+run Args {..} = either (error . show) return <=< runner $ do
     -- Grab all stake accounts
     stakes <- saResults <$> (getAccountStakes >>= raiseAPIError)
     -- Grab rewards for each stake account
@@ -70,4 +89,32 @@ run apiKey accountPubKey = either (error . show) return <=< runner $ do
         in  liftIO $ T.putStrLn output
   where
     runner :: ReaderT Config (ExceptT APIError IO) a -> IO (Either APIError a)
-    runner = runExceptT . flip runReaderT (mkConfig apiKey accountPubKey)
+    runner = runExceptT . flip runReaderT (mkConfig argApiKey argPubKey)
+
+
+-- | CLI arguments supported by the executable.
+data Args = Args
+    { argApiKey :: String
+    -- ^ Solana Beach API Key
+    , argPubKey :: String
+    -- ^ Delegator's PubKey.
+    }
+    deriving (Show, Read, Eq, Data, Typeable)
+
+-- | Parse the CLI arguments with 'System.Console.CmdArgs'.
+getArgs :: IO Args
+getArgs = cmdArgs argSpec
+
+argSpec :: Args
+argSpec =
+    Args { argApiKey = def &= argPos 0 &= typ "API_KEY"
+         , argPubKey = def &= argPos 1 &= typ "ACCOUNT_PUBKEY"
+         }
+        &= summary
+               (  "solana-staking-csvs v"
+               <> showVersion version
+               <> ", Pavan Rikhi 2021"
+               )
+        &= program "solana-staking-csvs"
+        &= helpArg [name "h"]
+        &= help "Generate CSV Exports of your Solana Staking Rewards."
